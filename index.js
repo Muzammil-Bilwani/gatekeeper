60ec5142ab4f76e1828a1711301a15fc1cf97092
Muzammil Bilwani <muzammil@brandver.se>
2022-10-19 23:08:35 +0500

    response beauty

--- index.js
+++ index.js
@@ -5,21 +5,30 @@ const app = express()
 const port = 3000
 
 app.get("/authenticate/:code", async (req, res) => {
-  const response = await axios.post(
-    "https://github.com/login/oauth/access_token",
-    {
-      client_id: "4f26ab3457765e21e424",
-      redirect_uri: "https://rabbitpull.com",
-      client_secret: "3cd0af8bcb69e8da15d9a4d1b353534700d4131c",
-      code: req.params.code
-    },
-    {
-      headers: {
-        accept: "application/json"
+  try {
+    const response = await axios.post(
+      "https://github.com/login/oauth/access_token",
+      {
+        client_id: "4f26ab3457765e21e424",
+        redirect_uri: "https://rabbitpull.com",
+        client_secret: "3cd0af8bcb69e8da15d9a4d1b353534700d4131c",
+        code: req.params.code
+      },
+      {
+        headers: {
+          accept: "application/json"
+        }
       }
-    }
-  )
-  return res.json(response.data)
+    )
+    if (response.data.access_token) return res.json(response.data)
+
+    return res.status(400).json({ error: "No access token" })
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({
+      message: error?.message || "Something went wrong"
+    })
+  }
 })
 
 app.listen(port, () => {
